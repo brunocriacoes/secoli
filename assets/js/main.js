421de938b094017cc1b5ea60c0f4c3be65215fd8
MaxTecnology <maxsouza150@hotmail.com>
2024-01-29 17:01:33 -0300

    Feature: agendar contato e form dialog

---
+++ assets/js/main.js
@@ -0,0 +1,12 @@
+
+
+function agendar_contato() {
+    const dialog = document.getElementById("favDialog");
+    dialog.showModal();
+
+}
+
+function close_contato() {
+    const dialog = document.getElementById("favDialog");
+    dialog.close();
+}
